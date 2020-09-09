e65d4679073163e74c27209e9b76aab75e9dcccb
pandeyvibhu <vibhupandey1995@gmail.com>
2020-09-09 23:44:37 +0530

    RDS test

--- src/main/java/com/example/dashbook/dashbook/HelloWorldController.java
+++ src/main/java/com/example/dashbook/dashbook/HelloWorldController.java
@@ -1,13 +1,31 @@
 package com.example.dashbook.dashbook;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.dashbook.dashbook.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class HelloWorldController {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String helloWorld() {
         return "Hello World!";
     }
+
+    @PostMapping("/createUser")
+    public void create(@RequestBody User user){
+        userRepository.save(user);
+    }
+
+
+    @GetMapping("/user/{id}")
+    public Optional<User> get(@PathVariable("id") int id){
+        return userRepository.findById(id);
+    }
+
 }
---
+++ src/main/java/com/example/dashbook/dashbook/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.dashbook.dashbook;
+
+import com.example.dashbook.dashbook.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
